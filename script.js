23deccc1c6337be336fa58908817b77a9f15fc86
matthewgani <matthewgani2@gmail.com>
2022-05-24 12:13:29 +0800

    Finish base js code and some html layout for calculator

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+function add(a, b) {
+    return a+b;
+}
+function subtract(a, b) {
+    return a-b;
+}
+function multiply(a, b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+
+function operate(a, b, operator) {
+    let result = 0;
+    switch(operator) {
+        case 'add':
+            result = add(a,b);
+            break;
+        case 'subtract':
+            result = subtract(a,b);
+            break;
+        case 'multiply':
+            result = multiply(a,b);
+            break;
+        case 'divide':
+            result = divide(a,b);
+            break;
+        default:
+            console.log('invalid operator');
+    }
+
+    return result
+}
